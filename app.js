5d19b36937dfdea6032c699b1bdfb8bb7437d042
Kevin Allred <16504353+TheAllred@users.noreply.github.com>
2023-06-10 10:20:22 -0600

    Auth0

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
+const dotenv = require("dotenv");
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const { auth } = require("express-openid-connect");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.secret,
+  baseURL: "https://duck-directory-api.onrender.com/",
+  clientID: "v9EMKJY95JYvlVb8UvH9UBmniMUSSSbn",
+  issuerBaseURL: "https://dev-ysgqikzna2aqfoi7.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 
 app
   .use(bodyParser.json())
--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,10 @@ const swaggerDocument = require("../swagger-output.json");
 router.use("/api-docs", swaggerUi.serve);
 router.get("/api-docs", swaggerUi.setup(swaggerDocument));
 
+const { requiresAuth } = require("express-openid-connect");
+
+router.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
 module.exports = router;
